1c47543485e781457b933976a1281d7de1d8e539
akorsos <alexander.w.korsos@gmail.com>
2015-04-16 10:21:22 -0500

    Commit 1
    Created and linked MySQL DB through NetBeans built in manager. Created
    entity classes for DB.

---
+++ ClassLib/build/generated-sources/ap-source-output/ak/entities/Person_.java
@@ -0,0 +1,15 @@
+package ak.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-16T10:15:58")
+@StaticMetamodel(Person.class)
+public class Person_ { 
+
+    public static volatile SingularAttribute<Person, String> firstname;
+    public static volatile SingularAttribute<Person, Integer> userid;
+    public static volatile SingularAttribute<Person, String> lastname;
+
+}
